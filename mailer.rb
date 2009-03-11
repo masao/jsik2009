2c2cd594fa572a5900ebf01616713e72adda9a5c
masao <tmasao@acm.org>
2009-03-11 06:54:54 +0000

    so-2022-jp では ERB を通すのに問題が起きるので、EUC-JPにして、あとで変換することにする。

--- mailer.rb
+++ mailer.rb
@@ -7,18 +7,19 @@
 require "erb"
 require "kconv"
 
-SENDMAIL = '/usr/sbin/sendmail'
-FROM = 'TAKAKU.Masao@nims.go.jp'
+NKF_command = '/usr/local/bin/nkf'
+SENDMAIL_command = '/usr/sbin/sendmail'
+FROM_address = 'TAKAKU.Masao@nims.go.jp'
 
 message = STDIN.read
 #puts message
 
 ARGF.each do |line|
-   data = line.chomp.tojis.split(/\t/)
+   data = line.chomp.split(/\t/)
    next if data.empty?
    result = ERB::new( message ).result( binding )
    puts data[7]
-   open("|#{SENDMAIL} -oi -t -f #{FROM}", "w") do |io|
+   open("|#{NKF_command} -j -m0 | #{SENDMAIL_command} -oi -t -f #{FROM_address}", "w") do |io|
       io.puts result
    end
 end
