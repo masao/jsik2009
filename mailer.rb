fbbfc0a023b5f5acd9bd8ceeb9b38a3ffcdda919
masao <tmasao@acm.org>
2009-03-11 06:04:01 +0000

    usage

--- mailer.rb
+++ mailer.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/env ruby
 # $Id$
 
+# Usage:
+#  ./mailer.rb data.txt < inbox
+
 require "erb"
 require "kconv"
 
@@ -9,9 +12,10 @@ FROM = 'TAKAKU.Masao@nims.go.jp'
 
 ARGF.each do |line|
    data = line.chomp.tojis.split(/\t/)
-   next if data.size <= 1
+   next if data.empty?
    message = ERB::new( STDIN.read ).result( binding )
    #puts message
+   puts data[7]
    open("|#{SENDMAIL} -oi -t -f #{FROM}", "w") do |io|
       io.puts message
    end
