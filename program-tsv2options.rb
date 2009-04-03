59bed29af3780b8ab5c47579a7b1f7e3366799c6
masao <tmasao@acm.org>
2009-04-03 02:42:18 +0000

    add

---
+++ program-tsv2options.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# -*- coding: euc-jp -*-
+# $Id$
+
+$KCODE = "euc"
+
+def shorten( str, len = 120 )
+   matched = str.gsub( /\n/, ' ' ).scan( /^.{0,#{len - 2}}/ )[0]
+   if $'.nil? || $'.empty?
+      matched
+   else
+      matched + '...'
+   end
+end
+
+ARGF.each do |line|
+   pid, title, author = line.chomp.split(/\t/)
+   title.strip!
+   author = author.gsub(/^\"(.*)\"$/, '\1').strip
+   #p [ pid, title, author ]
+   authors = author.split(/, /)
+   authors_str  =authors[0].split[0]
+   authors_str << "д█дл" if authors.size > 1
+   puts %Q[<option value="#{ pid.gsub(/-/,"") }">[#{ pid }] #{ authors_str }: #{ shorten(title,20) }</option>]
+end
